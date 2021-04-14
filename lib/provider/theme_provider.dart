25d4431b9303cbc1cfadee53488cdcfd0cf8d942
Jobin Johnson <jobinrjohnson@gmail.com>
2021-04-14 17:02:34 +0530

    added theme switching persistent

--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:pastebin/theme/dark.dart';
 import 'package:pastebin/theme/light.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider with ChangeNotifier {
+  late SharedPreferences prefs;
+
   int themeId = 0;
+
+  ThemeProvider() {
+    init();
+  }
+
+  init() async {
+    prefs = await SharedPreferences.getInstance();
+    this.themeId = prefs.getInt("themeId") ?? 0;
+    notifyListeners();
+  }
 
   ThemeData get theme {
     if (themeId == 0)
@@ -14,6 +27,7 @@ class ThemeProvider with ChangeNotifier {
 
   setTheme(int themeId) {
     this.themeId = themeId;
+    prefs.setInt("themeId", themeId);
     notifyListeners();
   }
 }
